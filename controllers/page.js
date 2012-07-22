e6ba3714b260b7eb7b804efab9e3f7cae2c2b051
Richard Willis <willis.rh@gmail.com>
2012-07-22 10:43:19 +0100

    Moving code into methods

--- controllers/page.js
+++ controllers/page.js
@@ -18,55 +18,55 @@ PageController.prototype.actionIndex = function() {
   return true;
 };
 
+PageController.prototype.getPage = function() {
+
+  var uri = (this.req.route.contentUri || this.req.url.replace('/', '')).replace(/\?.*$/, '');
+
+  // Load the page data record
+  var record = new DataStore('pages').where(function(page){
+    return page.uri == uri
+  }).find()[0];
+
+  if (!record) {
+    console.log('Page record not found for URI:', uri);
+    console.log('Route:', this.req.route);
+    this.res.send(404);
+    return;
+  }
+
+  return new PageModel( record );
+};
+
+PageController.prototype.getNavPages = function() {
+  return new DataStore('pages').where(function(page){
+    return !!page.showInNav;
+  }).find().map(function(data){
+    return new PageModel(data);
+  });
+};
+
 PageController.prototype.after = function() {
 
   BaseController.prototype.after.apply(this, arguments);
 
   if (this.page === undefined) {
-
-    var uri = (this.req.route.contentUri || this.req.url.replace('/', '')).replace(/\?.*$/, '');
-
-    // Load the page data record
-    var record = new DataStore('pages').where(function(page){
-      return page.uri == uri
-    }).find()[0];
-
-    if (!record) {
-      console.log('Page record not found for URI:', uri);
-      console.log('Route:', this.req.route);
-      this.res.send(404);
-      return;
-    }
-
-    // Load the page model
-    this.page = new PageModel( record );
+    this.page = this.getPage();
   }
 
-  // Update breadcrumbs
   this.breadcrumbs.push({
     uri: this.req.url,
     title: this.page.title,
     last: true
   });
 
-  // Load the navigation pages
-  var navPages = new DataStore('pages').where(function(page){
-    return !!page.showInNav;
-  }).find().map(function(data){
-    return new PageModel(data);
-  });
-
-  // Load the navigation view
   this.view.navigation = new View('fragments/navigation.mustache', { 
-    pages: navPages
+    pages: this.getNavPages()
   }).render();
 
-  // Load the breadcrumbs view
   this.view.breadcrumbs = new View('fragments/breadcrumbs.mustache', { 
     breadcrumbs: this.breadcrumbs
   }).render();
 
-  // Add data to view
   this.view.scripts = new View('fragments/scripts.mustache').render();
   this.view.page = this.page;
   this.view.controller = this.req.route.controller.charAt(0).toUpperCase() + this.req.route.controller.slice(1);
