8065e2cf68843c745ac77f1cd1e561ba5f30201e
Richard Willis <willis.rh@gmail.com>
2012-08-05 03:03:36 +0100

    Scroll the comments section into view once the disqus comments have loaded

--- public/js/app.js
+++ public/js/app.js
@@ -174,8 +174,21 @@ App.Controllers.Post.prototype.onViewCommentsClick = function(e) {
 };
 
 App.Controllers.Post.prototype.showDisqusComments = function() {
+  
+  window.disqus_config = this.disqusConfig;
+
   App.Util.globalizeConfig( App.Config.Disqus );
   App.Util.insertScript('disqus-comments', 'http://' + App.Config.Disqus.disqus_shortname + '.disqus.com/embed.js');
+};
+
+App.Controllers.Post.prototype.disqusConfig = function() {
+  this.callbacks.onReady.push(App.Controllers.Post.prototype.onDisqusReady);
+};
+
+App.Controllers.Post.prototype.onDisqusReady = function() {
+  $('html,body').animate({
+    scrollTop: $('#disqus_thread').offset().top - 70 
+  }, 800);
 };
 
 /**********************
