821d84267c1e1d2e5af47194adb73ffc5e73cb4e
Richard Willis <willis.rh@gmail.com>
2012-08-21 13:23:25 +0100

    Create Valid singleton to hold the validation functions

--- controllers/contact.js
+++ controllers/contact.js
@@ -17,21 +17,10 @@ ContactController.prototype.actionPost = function() {
 
   var validator = new Validator(data);
 
-  validator.rule('name', function(val) {
-    return val.length > 0;
-  }, '- must not be empty');
-
-  validator.rule('email', function(val) {
-    return val.length > 0;
-  }, '- must not be empty');
-
-  validator.rule('email', function(val) {
-    return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test( val );
-  }, '- must be a valid email');
-  
-  validator.rule('message', function(val) {
-    return val.length > 0;
-  }, '- must not be empty');
+  validator.rule('name', 'notEmpty', '- must not be empty');
+  validator.rule('email', 'notEmpty', '- must not be empty');
+  validator.rule('email', 'isEmail', '- must be a valid email');
+  validator.rule('message', 'notEmpty', '- must not be empty');
 
   var errors = validator.check();
 
